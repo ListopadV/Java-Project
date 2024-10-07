6de43758b8bd378fbb7b35f06202fcfbdca78aea
Listopadiiiik <vladlistopad6@gmail.com>
2024-10-07 10:44:35 +0200

    s30472 added current task

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Added adder = new Adder();
+        System.out.println(added.add(1, 2));
+        Substractor substractor = new Substractor();
+        System.out.println(substractor.substract(6, 3));
+    }
+}
